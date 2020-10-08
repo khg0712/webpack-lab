95e38131d5247567b9360614f39179277163df55
김형규 <khg0712@midasit.com>
2020-10-08 18:59:47 +0900

    css import 빌드 시도

--- mpa-lab/src/a.js
+++ mpa-lab/src/a.js
@@ -1,5 +1,9 @@
 import { a } from "./common1";
 import { k } from "./common2";
+import style from './common.css';
 
 console.log(a);
 console.log(k);
+const el = document.createElement("div");
+el.className = "container";
+document.body.append(el);
--- mpa-lab/webpack.config.js
+++ mpa-lab/webpack.config.js
@@ -1,22 +1,48 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ChunksWebpackPlugin = require("chunks-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
+const commonStyle = "./src/common.css";
+
 module.exports = {
 	entry: {
-		a: "./src/a.js",
-		b: "./src/b.js",
-		c: "./src/c.js",
+		a: ["./src/a.js"],
+		b: ["./src/b.js"],
+		c: ["./src/c.js"],
 	},
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "[name].[hash].bundle.js",
 	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
+			},
+		],
+	},
 	optimization: {
+		namedChunks: false,
+		namedModules: false,
+		removeAvailableModules: true,
+		removeEmptyChunks: true,
+		mergeDuplicateChunks: true,
+		occurrenceOrder: true,
+		providedExports: false,
 		splitChunks: {
 			chunks: "all",
 			minSize: 0,
+			name: true,
 		},
 	},
-	plugins: [new CleanWebpackPlugin(), new ChunksWebpackPlugin()],
+	plugins: [
+		new CleanWebpackPlugin(),
+		new ChunksWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			chunkFilename: `[id].[contenthash].css`,
+			moduleFilename: ({ name }) => `${name}.[contenthash].css`,
+		}),
+	],
 };
